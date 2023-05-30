5888e44e929cd3b1abc82323f03417846d2d52a7
ELS <57393100+kodosa7@users.noreply.github.com>
2023-05-30 16:32:21 +0200

    reduced text

--- dist/components/Footer.jsx
+++ dist/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="m-auto w-2/4 text-center border-gray-800 text-white border-t-2 mt-14">
             </div>
             <h3 className="text-gray-500 text-lg text-center mt-10 text-sm">
-                <strong>Dev BugFlow</strong> | Developed by ELS 2023 | Built with ReactJS & TailwindCSS
+                <strong>Dev BugFlow</strong> | ELS 2023 | ReactJS & TailwindCSS
             </h3>
         </>
     )
