b7ac2988c85b7fe517b5ab8341b90bd1b915a8ae
ELS <57393100+kodosa7@users.noreply.github.com>
2023-06-06 14:09:41 +0200

    time/date centered to the place of footer message
    fixed delete bug of the 1st item

--- dist/components/Main.jsx
+++ dist/components/Main.jsx
@@ -1,155 +1,3 @@
-// import React, { useState } from "react";
-// import Upload from "./Upload";
-// import Counter from "./Counter";
-// import Buttons from "./Buttons";
-
-// export default function Main() {
-//   const [steps, setSteps] = useState([{ id: 1, isImagePasted: false }]);
-
-//   const handleAddNextStep = () => {
-//     const newStep = {
-//       id: steps.length + 1,
-//       isImagePasted: false,
-//     };
-//     setSteps((prevSteps) => [...prevSteps, newStep]);
-//   };
-
-//   const handleImagePasted = (stepId) => {
-//     setSteps((prevSteps) => {
-//       return prevSteps.map((step) => {
-//         if (step.id === stepId) {
-//           return {
-//             ...step,
-//             isImagePasted: true,
-//           };
-//         }
-//         return step;
-//       });
-//     });
-//   };
-
-//   const handleDeleteStep = (stepId) => {
-//     setSteps((prevSteps) => {
-//       const remainingSteps = prevSteps.filter((step) => step.id !== stepId);
-  
-//       if (remainingSteps.length === 0) {
-//         // If the last remaining step is deleted or there is only one step and it's the first one, reset the app to the initial state
-//         return [{ id: 1, isImagePasted: false }];
-//       }
-  
-//       let updatedSteps;
-  
-//       if (stepId === 1) {
-//         updatedSteps = remainingSteps.map((step, index) => ({
-//           ...step,
-//           id: index + 1,
-//         }));
-//       } else {
-//         updatedSteps = remainingSteps.map((step, index) => ({
-//           ...step,
-//           id: index < stepId - 1 ? index + 1 : index + 2,
-//         }));
-//       }
-  
-//       return updatedSteps;
-//     });
-//   };
-  
-//   // working
-//   const handleRemoveStep = (stepId) => {
-//     console.log("steps.length", steps.length);
-//     console.log("stepId", stepId);
-
-//     if (steps.length === 1) {
-//       console.log("setps.length is 1, resetting")
-//       setSteps([{ id: 0, isImagePasted: false }]); // Set the initial state
-
-//     } else {
-//       console.log("steps.length above 1, continuing")
-//       setSteps((prevSteps) => {
-//         return prevSteps.filter((step) => step.id !== stepId);
-//       });
-//     };
-//   };
-
-//   // was correct
-
-//   // const handleDeleteStep = (stepId) => {
-//   //   setSteps((prevSteps) => {
-//   //     const remainingSteps = prevSteps.filter((step) => step.id !== stepId);
-  
-//   //     if (remainingSteps.length === 0) {
-//   //       // If all steps are deleted, reset the app to initial state
-//   //       return [{ id: 1, isImagePasted: false }];
-//   //     }
-  
-//   //     const updatedSteps = remainingSteps.map((step, index) => ({
-//   //       ...step,
-//   //       id: index < stepId - 1 ? index + 1 : index + 2,
-//   //     }));
-  
-//   //     return updatedSteps;
-//   //   });
-//   // };
-
-//   const handleDeleteImage = (stepId) => {
-//     setSteps((prevSteps) => {
-//       return prevSteps.map((step) => {
-//         if (step.id === stepId) {
-//           return {
-//             ...step,
-//             isImagePasted: false,
-//           };
-//         }
-//         return step;
-//       });
-//     });
-//   };
-
-//   const isLastStep = (stepId) => {
-//     return stepId === steps.length;
-//   };
-
-//   return (
-//     <div className="">
-//       {steps.map((step, index) => (
-//         <div key={step.id} className="main-container my-0 mx-auto w-11/12 bg-gray-800 mb-5 rounded">
-//           <div className="counter-container">
-//             <Counter counter={index + 1} />
-
-//             {step.isImagePasted && (
-//               <button
-//                 className="delete-button button-animation py-2 px-4 ml-5 bg-gray-900 text-gray-400 hover:bg-gray-700 transition-all duration-75 font-bold rounded shadow-xl"
-//                 onClick={() => handleRemoveStep(step.id)}
-//                 data-html2canvas-ignore="true"
-//               >
-//                 Remove step
-//               </button>
-//             )}
-//           </div>
-
-//           <Upload
-//             handleAddNextStep={isLastStep(step.id) ? handleAddNextStep : undefined}
-//             handleImagePasted={() => handleImagePasted(step.id)}
-//             handleDeleteImage={() => handleDeleteImage(step.id)}
-//             isImagePasted={step.isImagePasted}
-//           />
-
-//           {(isLastStep(step.id) || steps.length === 1) && step.isImagePasted && (
-//             <div className="buttons-container" data-html2canvas-ignore="true">
-//               <Buttons
-//                 handleAddNextStep={handleAddNextStep}
-//                 isImagePasted={step.isImagePasted}
-//               />
-//             </div>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// ================
 import React, { useState } from "react";
 import Upload from "./Upload";
 import Counter from "./Counter";
@@ -184,12 +32,52 @@ export default function Main() {
     });
   };
 
+  // const handleRemoveStep = (stepId) => {
+  //   setSteps((prevSteps) => {
+  //     return prevSteps.filter((step) => step.id !== stepId);
+  //   });
+  //   setIsFooterVisible(true); // Show the footer after removing a step
+  // };
+
+
+    // working
   const handleRemoveStep = (stepId) => {
-    setSteps((prevSteps) => {
-      return prevSteps.filter((step) => step.id !== stepId);
-    });
+    console.log("steps.length", steps.length);
+    console.log("stepId", stepId);
     setIsFooterVisible(true); // Show the footer after removing a step
+
+    if (steps.length === 1) {
+      console.log("setps.length is 1, resetting")
+      setSteps([{ id: 0, isImagePasted: false }]); // Set the initial state
+
+    } else {
+      console.log("steps.length above 1, continuing")
+      setSteps((prevSteps) => {
+        return prevSteps.filter((step) => step.id !== stepId);
+      });
+    };
   };
+
+  // was correct
+
+  // const handleDeleteStep = (stepId) => {
+  //   setSteps((prevSteps) => {
+  //     const remainingSteps = prevSteps.filter((step) => step.id !== stepId);
+  
+  //     if (remainingSteps.length === 0) {
+  //       // If all steps are deleted, reset the app to initial state
+  //       return [{ id: 1, isImagePasted: false }];
+  //     }
+  
+  //     const updatedSteps = remainingSteps.map((step, index) => ({
+  //       ...step,
+  //       id: index < stepId - 1 ? index + 1 : index + 2,
+  //     }));
+  
+  //     return updatedSteps;
+  //   });
+  // };
+
 
   const handleDeleteImage = (stepId) => {
     setSteps((prevSteps) => {
@@ -255,7 +143,7 @@ export default function Main() {
         </div>
       ))}
       
-      {isFooterVisible ? <Footer /> : <Time />} {/* Render the Footer component only if isFooterVisible is true */}
+      {isFooterVisible ? <Footer /> : <Time />}
 
     </div>
   );
--- dist/components/Time.jsx
+++ dist/components/Time.jsx
@@ -10,9 +10,9 @@ export default function Time() {
 
     return (
         <>
-            {/* <div className="m-auto w-2/4 text-center border-gray-800 text-white border-t-2 mt-14">
-            </div> */}
-            <h3 className="text-gray-500 text-lg text-left my-10 mx-10 text-sm pb-10">
+            <div className="m-auto w-2/4 text-center border-gray-800 text-white border-t-2 mt-14">
+            </div>
+            <h3 className="text-gray-500 text-lg text-center my-10 mx-10 text-sm pb-10">
                 Flow rendered on <strong>{ showTime }</strong>
             </h3>
         </>
