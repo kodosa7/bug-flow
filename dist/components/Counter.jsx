3b31c670f3567286e5effd05c9fc41b3824a7c84
ELS <57393100+kodosa7@users.noreply.github.com>
2023-06-08 08:59:57 +0200

    fixed width Counter box & Remove button
    fixed width Counter box & Remove button to fit when count >= 10

--- dist/components/Counter.jsx
+++ dist/components/Counter.jsx
@@ -1,6 +1,6 @@
 export default function Counter({ counter }) {
   return (
-    <div className="text-6xl font-bold pl-12 pr-12 pt-5 pb-7 ml-5 mb-5 mt-5 w-min h-max bg-gray-900 text-gray-200 rounded-lg shadow-xl flex flex-col items-center">
+    <div className="text-6xl font-bold w-36 py-12 pt-5 pb-7 ml-5 mb-5 mt-5  bg-gray-900 text-gray-200 rounded-lg shadow-xl flex flex-col items-center">
       <div className="text-lg flex justify-center">step</div>
       {counter}
     </div>
