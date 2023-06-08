22c6fc3706de6ab2c34865fedf1aac57786ead2a
ELS <57393100+kodosa7@users.noreply.github.com>
2023-06-08 08:57:56 +0200

    Delete Counter.jsx

--- Counter.jsx
+++
@@ -1,8 +0,0 @@
-export default function Counter({ counter }) {
-  return (
-    <div className="text-6xl font-bold w-36 py-12 pt-5 pb-7 ml-5 mb-5 mt-5  bg-gray-900 text-gray-200 rounded-lg shadow-xl flex flex-col items-center">
-      <div className="text-lg flex justify-center">step</div>
-      {counter}
-    </div>
-  );
-}
