90c00e8c7be1349a1dc840ae896ac338c343e559
ELS <57393100+kodosa7@users.noreply.github.com>
2023-06-12 10:37:30 +0200

    Clickable header to refresh page when clicked

--- dist/components/Header.jsx
+++ dist/components/Header.jsx
@@ -1,13 +1,12 @@
 import bugLogo from "/dist/assets/bug.png";
 
-
 export default function Header() {
     return (
-        <div>
+        <a href="">
             <div className="header text-gray-100 text-4xl font-extrabold text-center p-10">
             <img src={bugLogo} alt="Logo" className="logo h-10 mr-3 mb-2" />
                 Dev BugFlow
             </div>
-        </div>
+        </a>
     )
 }
