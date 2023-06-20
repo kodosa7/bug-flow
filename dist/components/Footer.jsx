5ee556396485b95d03ffd53631feec391720b8bb
ELS <57393100+kodosa7@users.noreply.github.com>
2023-06-20 10:07:54 +0200

    added links to Footer

--- dist/components/Footer.jsx
+++ dist/components/Footer.jsx
@@ -3,7 +3,16 @@ export default function Footer() {
         <>
             <div className="m-auto w-2/4 text-center border-gray-800 text-white border-t-2 mt-14"></div>
             <h3 className="footer text-gray-500 text-center my-10 pb-10 text-sm">
-                <strong>Dev BugFlow</strong> | ELS 2023 | ReactJS & TailwindCSS
+                <strong><a href="">Dev BugFlow</a></strong> |&nbsp;
+                <a href="https://github.com/kodosa7/bug-flow"
+                   target="_blank"
+                   rel="noreferrer">ELS</a> 2023 |&nbsp;
+                <a href="https://react.dev/"
+                   target="_blank"
+                   rel="noreferrer">ReactJS</a> &&nbsp;
+                <a href="https://tailwindcss.com/"
+                   target="_blank"
+                   rel="noreferrer">TailwindCSS</a>
             </h3>
         </>
     )
